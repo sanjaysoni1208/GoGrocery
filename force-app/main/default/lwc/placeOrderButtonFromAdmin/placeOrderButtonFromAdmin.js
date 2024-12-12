5a97335deaeb4954fd6815a09e8413188a770ae6
Deba951 <debarghya.chakravarty@gmail.com>
2024-12-12 23:03:31 +0530

    Cart to Order Button

---
+++ force-app/main/default/lwc/placeOrderButtonFromAdmin/placeOrderButtonFromAdmin.js
@@ -0,0 +1,23 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import createOrder from '@salesforce/apex/OrderCreationHandler.createOrder';
+
+export default class NavigateToOrderPage extends NavigationMixin(LightningElement) {
+    handlePlaceOrder() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Order__c',
+                actionName: 'new'
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
--- force-app/main/default/triggers/ApplyVoucherDiscount.trigger
+++ force-app/main/default/triggers/ApplyVoucherDiscount.trigger
@@ -1,9 +1,10 @@
-trigger ApplyVoucherDiscount on Order__c (before insert, before update, after update) {
+trigger ApplyVoucherDiscount on Order__c (before insert, before update, after insert, after update) {
+
     if (Trigger.isBefore) {
         VoucherDiscountHandler.applyVoucherDiscount(Trigger.new);
     }
-
-    if(Trigger.isAfter) {
-        VoucherDiscountHandler.sendVoucherNotification(Trigger.new);
+    
+    if (Trigger.isAfter) {
+        OrderStatusHandler.updateOrderStatus(Trigger.new);
     }
 }
